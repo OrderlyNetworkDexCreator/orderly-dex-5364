2ac62c4b56b1562ce643c546e020d09daec0be85
Mario Reder <mario.reder@pm.me>
2026-03-16 01:18:32 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__RUNTIME_CONFIG__ = {
   "VITE_ORDERLY_BROKER_ID": "demo",
-  "VITE_ORDERLY_BROKER_NAME": "acct2_massassign_probe",
+  "VITE_ORDERLY_BROKER_NAME": "s5-traversal",
   "VITE_BROKER_EOA_ADDRESS": "0x67a3585059e02fc7f3a62458f078501d7cb20941",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
@@ -14,7 +14,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_DISABLE_EVM_WALLETS": "false",
   "VITE_DISABLE_SOLANA_WALLETS": "false",
   "VITE_WALLETCONNECT_PROJECT_ID": "",
-  "VITE_APP_NAME": "acct2_massassign_probe",
+  "VITE_APP_NAME": "s5-traversal",
   "VITE_APP_DESCRIPTION": "Orderly Trading Application",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
